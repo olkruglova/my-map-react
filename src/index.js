ef12ece7fb7de1ecac77685f37a2670a72340812
olkruglova <olkruhlova@gmail.com>
2018-08-05 00:35:02 +0200

    Add a Sidebar

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Map from './Components/Map.js';
+import Sidebar from './Components/Sidebar.js';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Map />, document.getElementById('map'));
+ReactDOM.render(<Sidebar />, document.getElementById('sidebar'));
 registerServiceWorker();
